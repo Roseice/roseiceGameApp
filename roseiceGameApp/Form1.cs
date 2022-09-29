b42de4ca33bf5ebc33075a4533e9db281d2d01f4
Roseice <roseice910528@gmail.com>
2022-09-29 11:46:36 +0800

    9/29roseice
    k

--- roseiceGameApp/Form1.cs
+++ roseiceGameApp/Form1.cs
@@ -7,9 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
+//使用通訊協定和執行此類方法
+using System.Net;//網路通訊協定
+using System.Net.Sockets;//網路socket方法
+using System.Threading;//多執行續方法
 
 namespace roseiceGameApp
 {
@@ -20,7 +21,7 @@ namespace roseiceGameApp
         public Form1()
         {
             InitializeComponent();
-            this.Text = "IP:" + findhostIP();
+            this.Text = "IP:" + FindhostIP();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -31,6 +32,25 @@ namespace roseiceGameApp
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+        //定義方法FindhostIP查找ip
+        private string FindhostIP()
+        {
+            string hostIP = "";
+            string hostName = Dns.GetHostName();//得到主機名稱
+            //得到主機ip位置清單
+            IPAddress[]iPs= Dns.GetHostEntry(hostName).AddressList;
+            //使用foreach迴圈--比對是否有IPV4,如果有就回傳IPv4字串
+            foreach(IPAddress x in iPs)
+            {
+                if (x.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    hostIP = x.ToString();//取得IPv4字串
+                    return (hostIP);//傳回本機IPv4字串
+                }
+            }
+            //如果沒有找到IPv4就傳回空字串
+            return ("");
         }
     }
 }
